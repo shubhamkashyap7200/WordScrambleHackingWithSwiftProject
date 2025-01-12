8160575e26eddd1ba047f0ee989b9cc395895f1d
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-12 14:28:04 +0530

    Initial Commit

---
+++ WordScrambleHackingWithSwiftProject/WordScrambleHackingWithSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleHackingWithSwiftProjectApp.swift
+//  WordScrambleHackingWithSwiftProject
+//
+//  Created by Shubham on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleHackingWithSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
